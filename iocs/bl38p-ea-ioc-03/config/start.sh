ca199e5bdc83859d45722bbc5e86fea87d3bce4e
Giles Knap <giles.knap@diamond.ac.uk>
2023-10-24 11:05:37 +0100

    update panda IOC to latest

--- iocs/bl38p-ea-ioc-03/config/start.sh
+++ iocs/bl38p-ea-ioc-03/config/start.sh
@@ -3,5 +3,5 @@
 # Create the OPI directory
 mkdir -p /epics/opi
 # Start the Panda soft IOC application
-pandablocks-ioc softioc bl38p-mo-panda-01 BL38P-EA-IOC-03 --clear-bobfiles --screens-dir /epics/opis
+pandablocks-ioc softioc bl38p-mo-panda-01 BL38P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opis
 
