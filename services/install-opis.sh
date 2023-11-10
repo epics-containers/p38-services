02ff49440ffa7782854ecce527170a75efe5c654
Giles Knap <giles.knap@diamond.ac.uk>
2023-11-10 16:03:35 +0000

    update services chart

--- services/install-opis.sh
+++ services/install-opis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Install a nginx server to publish GUI files for the beamline
-helm upgrade --install opis oci://ghcr.io/epics-containers/domain-opis \
-     --version 2.0.0 --set beamline=bl38p
+# helm upgrade --install opis oci://ghcr.io/epics-containers/domain-opis \
+#      --version 1.1.0 --set beamline=bl38p
+
+helm upgrade --install opis /repos/ec-helm-charts/Charts/opis \
+     --version 1.1.1 --set beamline=bl38p
