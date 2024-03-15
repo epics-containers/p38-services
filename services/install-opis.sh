def015821df0afc7b2eded335fad352a27cb4814
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-15 07:39:04 +0000

    update ioc instances to latest template

--- services/install-opis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install a nginx server to publish GUI files for the beamline
-helm upgrade --install opis oci://ghcr.io/epics-containers/domain-opis \
-     --version 1.0.0 --set beamline=bl38p
